d45b1d0cd59f621fd74f71c97eda4351126c3986
laura <justlxurx0@gmail.com>
2023-08-31 15:29:24 +0600

    dev practice section is done

--- src/app/education/page.tsx
+++ src/app/education/page.tsx
@@ -1,7 +1,7 @@
 
 import JoinUsFooter from '@/components/JoinUsFooter'
 import DevPractice from '@/components/DevPractice'
-
+import Navbar from '@/components/Navbar'
 //import TeamMember from '@/components/TeamMember'
 
 import styles from './page.module.scss'
@@ -9,7 +9,7 @@ import styles from './page.module.scss'
 export default async function Education() {
   return (
     <>
-    
+      <Navbar />
       <main>
         <DevPractice/>
       </main>
