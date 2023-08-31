744bc46cf13fc33d6bd5de1a1b05988ca1dbfa7a
laura <justlxurx0@gmail.com>
2023-08-31 17:30:01 +0600

    new section is in progress which is called Work

--- src/app/education/page.tsx
+++ src/app/education/page.tsx
@@ -3,7 +3,7 @@ import JoinUsFooter from '@/components/JoinUsFooter'
 import DevPractice from '@/components/DevPractice'
 import Navbar from '@/components/Navbar'
 //import TeamMember from '@/components/TeamMember'
-
+import Work from '@/components/Work'
 import styles from './page.module.scss'
 
 export default async function Education() {
@@ -12,6 +12,7 @@ export default async function Education() {
       <Navbar />
       <main>
         <DevPractice/>
+        <Work />
       </main>
       <JoinUsFooter />
     </>
