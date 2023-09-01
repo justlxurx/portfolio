b57efb3f28fb3c9fd54882d3b69b7a6ec3263b36
laura <justlxurx0@gmail.com>
2023-09-01 17:42:58 +0600

    new component which is named as BeProgrammer is ready

---
+++ src/components/BeProgrammer/index.tsx
@@ -0,0 +1,50 @@
+// import Button from '@/components/ui/Button'
+import styles from './styles.module.scss'
+import Image from 'next/image'
+import Key from 'public/Key.svg'
+import buttons from 'public/buttons.svg'
+
+const Programmer = () => {
+  return (
+     <section className={styles.be_programmer}>
+       <div className={styles.nav1}></div>
+       <div className={styles.be_programmer_main}>
+            <div className={styles.main1}>
+                <div className={styles.main1_column1}>
+              
+                    <p>Стать востребованным <br />разработчиком</p>
+                    <button>
+                      <Image src={Key}
+                      alt="button"/>
+                    </button>
+                </div>
+                <div className={styles.main1_column2}>
+                    <div className={styles.main1_column2_buttons}>
+                    <Image
+                      className={styles.main1_column2_buttons_img}
+                      src={buttons}
+                      alt="Shift + F6"
+                    />
+                    </div>
+                    <div className={styles.main1_column2_p}>
+                        <p>В конце обучения студенты получат <b>сертификаты и рекомендательные письма</b> <br />для будущих работодателей.</p>
+                    </div>
+                </div>
+            </div>
+
+            <div className={styles.ab_lines}>
+              <div></div>
+              <div className={styles.a}></div>
+              <div className={styles.b}></div>
+            </div>
+
+       </div>
+    </section>
+  )
+}
+
+
+
+
+
+export default Programmer
