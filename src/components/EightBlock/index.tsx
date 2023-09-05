6bf1769406e3e2cb008889b1bf784d921b414042
laura <justlxurx0@gmail.com>
2023-09-05 17:41:19 +0600

    in progress scroller section

--- src/components/EightBlock/index.tsx
+++ src/components/EightBlock/index.tsx
@@ -1,9 +1,27 @@
 'use client'
 import Image from 'next/image'
 import styles from './styles.module.scss'
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef, RefObject } from 'react';
 import classNames from 'classnames';
 import arrow_button from 'public/arrow_button.svg'
+
+
+function isInViewport(elementRef: RefObject<HTMLElement>): boolean {
+  if (!elementRef || !elementRef.current) {
+    return false;
+  }
+
+  const element = elementRef.current;
+  const rect = element.getBoundingClientRect();
+
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <=
+      (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
 
 type BlockProps = {
   title: string;
@@ -12,17 +30,8 @@ type BlockProps = {
   onClick: () => void;
 };
 
-// const Block = ({ title, description }: { title: string, description: string }) => {
-//     const [showInfo, setShowInfo] = useState(false);
-//     const [bottomColor, setBottomColor] = useState(''); 
 const Block = ({ title, description, showInfo, onClick }: BlockProps) => {
-  const [bottomColor, setBottomColor] = useState('');
-
-
-    const changeBottomColor = () => {
-        const сolor = "#E97810";
-        setBottomColor(сolor);
-      };
+   
   
     return (
       <div className={classNames(styles.scroll_blocks, { show: showInfo })}>
@@ -33,38 +42,78 @@ const Block = ({ title, description, showInfo, onClick }: BlockProps) => {
           </div>
         ) : (
             <div>
-          <button onClick={() => {onClick(); changeBottomColor();}} >
+          <button onClick={() => {onClick()}} >
             Подробнее
             <Image src={arrow_button} alt="arrow" />
           </button>
           
            </div>
-        )}<div className={styles.bottom_color} style={{ backgroundColor: bottomColor }}></div>
+        )}<div className={styles.bottom_color} ></div>
         </div>
     );
   };
 
+
+
+
 const Blocks = () => {
-    const [scrollTop, setScrollTop] = useState(0);
-    const [blockStates, setBlockStates] = useState<Array<boolean>>(Array(8).fill(false));
-    const [bottomColor, setBottomColor] = useState('');
+  const [blockStates, setBlockStates] = useState<Array<boolean>>(Array(9).fill(false));
+  const blockRef = useRef(null);
+ // const [isInView, setIsInView] = useState(false);
+  const [isVisible, setIsVisible] = useState(false);
+  const [visibleBlockCount, setVisibleBlockCount] = useState<number>(0);
+
   
-    const handleBlockClick = (index: number) => {
-      const updatedBlockStates = blockStates.map((state, i) => (i === index ? !state : false));
-      setBlockStates(updatedBlockStates);
-      setBottomColor(updatedBlockStates[index] ? "#E97810" : ''); // Устанавливаем цвет в зависимости от состояния
+
+
+  useEffect(() => {
+    const handleScroll = () => {
+    //  setIsInView(isInViewport(blockRef));
+      setIsVisible(isInViewport(blockRef));
     };
 
-    return (
-        
-       <section className={styles.learn_program}>
+     if (isVisible) {
+      // Отключаем скроллинг страницы, когда блок виден
+      document.body.style.overflow = 'hidden';
+    } else {
+      // Восстанавливаем обычный скроллинг, когда блок не виден
+      document.body.style.overflow = 'auto';
+    }
+
+    // Добавляем обработчик события прокрутки
+    window.addEventListener("scroll", handleScroll);
+
+    // Проверяем при загрузке страницы
+    handleScroll();
+
+    // Убираем обработчик события при размонтировании компонента
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [isVisible]);
+
+
+  
+    
+   
+   
+   
+   
+   const handleBlockClick = (index: number) => {
+      const updatedBlockStates = blockStates.map((state, i) => (i === index ? !state : false));
+      setBlockStates(updatedBlockStates);
+    };
+
+    return (      
+       <div>
+       <section className={styles.learn_program} >
             <div className={styles.learn_program_heading}>
                 <div></div>
                 <h1>программа обучения</h1>
                 <div></div>
             </div>
 
-            <div className={styles.learn_program_main}>
+            <div className={styles.learn_program_main} ref={blockRef}>
                 <h2>8 блоков обучения</h2>
                 <p>Интенсив захватывает основы языка <br /> программирования Java, работу <br />
                 с различными инструментами <br />
@@ -73,7 +122,7 @@ const Blocks = () => {
                     <div className={styles.learn_program_main_div_img}></div>
                     <div className={styles.learn_program_main_div_scroll} >
 
-                    <Block
+            <Block
               title="Java Core"
               description="База, которая необходима для старта. На интенсиве изучаем основы программирования и на практике 
               работаем с Java Core: интеграции с другими технологиями, 
@@ -99,55 +148,55 @@ const Blocks = () => {
           <Block
               title="Spring Core & MVC"
               description="Описание для Java Spring."
-              showInfo={blockStates[1]}
-              onClick={() => handleBlockClick(1)}
+              showInfo={blockStates[3]}
+              onClick={() => handleBlockClick(3)}
             />
 
             <Block
               title="Spring REST API"
               description="Описание для Elasticsearch."
-              showInfo={blockStates[2]}
-              onClick={() => handleBlockClick(2)}
+              showInfo={blockStates[4]}
+              onClick={() => handleBlockClick(4)}
             />
 
             <Block
               title="Spring OpenAPI"
               description="Описание для Java Spring."
-              showInfo={blockStates[1]}
-              onClick={() => handleBlockClick(1)}
+              showInfo={blockStates[5]}
+              onClick={() => handleBlockClick(5)}
             />
 
             <Block
               title="JUnit Tests"
               description="Описание для Elasticsearch."
-              showInfo={blockStates[2]}
-              onClick={() => handleBlockClick(2)}
+              showInfo={blockStates[6]}
+              onClick={() => handleBlockClick(6)}
             />
              <Block
               title="Spring Data"
               description="Описание для Java Spring."
-              showInfo={blockStates[1]}
-              onClick={() => handleBlockClick(1)}
+              showInfo={blockStates[7]}
+              onClick={() => handleBlockClick(7)}
             />
 
             <Block
               title="Spring Security"
               description="Описание для Elasticsearch."
-              showInfo={blockStates[2]}
-              onClick={() => handleBlockClick(2)}
+              showInfo={blockStates[8]}
+              onClick={() => handleBlockClick(8)}
             />
-
-                    </div>
+                  </div>
                 </div>
             </div>
-       </section>
-    )}
+           
+       </section> 
+      </div>  
+    )
+  
+  }
 
 
     export default Blocks 
 
 
 
-
-
-    
