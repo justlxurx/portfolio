0fb7e0ff55c44d1af760cd85982f4a6a07f343f0
laura <justlxurx0@gmail.com>
2023-09-12 15:37:45 +0600

    Intensive Details buttons to the left and right side are done

--- src/components/Details/index.tsx
+++ src/components/Details/index.tsx
@@ -1,132 +1,196 @@
 'use client'
-import React, { useEffect } from 'react';
+import React, { useRef, useState } from 'react'
+import { nanoid } from 'nanoid'
+
 import styles from './styles.module.scss'
+const ROADMAP_STEP = 450
 
 const Details = () => {
+  const [setctionPosition, setSectionPosition] = useState(0)
+
+  const [isRightButtonDisabled, setIsRightButtonDisabled] = useState(false)
+
+  const lastSectionRef = useRef<HTMLDivElement | null>(null)
 
   const handleLeftButtonClick = () => {
-    console.log('Left button clicked');
-    const scrollContainer = document.querySelector('.intensive_details__main__display');
-    scrollContainer?.classList.remove('moveRight');
-    scrollContainer?.classList.add('moveLeft');
-
-    if (scrollContainer?.classList.contains('moveLeft')) {
-      // Класс 'moveLeft' добавлен к элементу.
-      console.log('Класс moveLeft добавлен');
-    } else {
-      // Класс 'moveLeft' не добавлен к элементу.
-      console.log('Класс moveLeft не добавлен');
+    if (setctionPosition === 0) {
+      return
     }
-  };
+
+    setSectionPosition(setctionPosition + ROADMAP_STEP)
+  }
 
   const handleRightButtonClick = () => {
-    console.log('Right button clicked');
-    const scrollContainer = document.querySelector('.intensive_details__main__display');
-    scrollContainer?.classList.add('moveRight');
-    scrollContainer?.classList.remove('moveLeft');
+    if (!isRightButtonDisabled) {
+      setIsRightButtonDisabled(true)
 
-    if (scrollContainer?.classList.contains('moveLeft')) {
-      // Класс 'moveLeft' добавлен к элементу.
-      console.log('Класс moveLeft добавлен');
-    } else {
-      // Класс 'moveLeft' не добавлен к элементу.
-      console.log('Класс moveLeft не добавлен');
+      setTimeout(() => {
+        setIsRightButtonDisabled(false)
+      }, 1000)
     }
-  };
 
-  useEffect(() => {
-    const leftButton = document.querySelector('.button_to_left');
-    const rightButton = document.querySelector('.button_to_right');
+    const checkVisibility = () => {
+      let isVisible = false
+      if (lastSectionRef.current) {
+        const refRect = lastSectionRef.current.getBoundingClientRect()
+        const windowWidth = window.innerWidth
 
-    leftButton?.addEventListener('click', handleLeftButtonClick);
-    rightButton?.addEventListener('click', handleRightButtonClick);
+        isVisible = refRect.right < windowWidth
+      }
 
-    return () => {
-      leftButton!.removeEventListener('click', handleLeftButtonClick);
-      rightButton!.removeEventListener('click', handleRightButtonClick);
-    };
+      return isVisible
+    }
 
-  }, []);
+    const isVisible = checkVisibility()
+
+    if (isVisible) {
+      return
+    }
+
+    setSectionPosition(setctionPosition - ROADMAP_STEP)
+  }
+
+  const details = [
+    {
+      title: 'Java Core',
+      content: (
+        <>
+          {'<'}Code conventions <br /> <br />
+          {'<'}Типы данных, переменные и управляющие конструкции
+          <br /> <br />
+          {'<'}Массивы и коллекции & ООП: инкапсуляция, наследование,
+          полиморфизм и абстракция
+          <br /> <br />
+          {'<'}Исключения
+          <br /> <br />
+          {'<'}Потоки. Использование ключевого слова synchronized и методы wait,
+          <br /> <br />
+          {'<'}notify, notifyAll
+          <br />
+          <br />
+          {'<'}Ввод/вывод (I/0)
+          <br />
+          <br />
+          {'<'}Generics
+          <br />
+          <br />
+        </>
+      ),
+    },
+    {
+      title: 'Elasticsearch',
+      content: (
+        <>
+          {'<'}Работа с индексами и документами
+          <br /> <br />
+          {'<'}Поиск данных: ключевые слова, фразы, диапазоны значений
+          <br /> <br />
+          {'<'}Фильтры и агрегации - уточнение поискового запроса
+          <br /> <br />
+          {'<'}Многоязычный поиск
+          <br /> <br />
+          {'<'}Поиск с поддержкой релевантности
+          <br /> <br />
+        </>
+      ),
+    },
+    {
+      title: 'Spring Core & MVC',
+      content: (
+        <>
+          {'<'}Inversion of Control <br />
+          <br />
+          {'<'}Dependecy Injection <br />
+          <br />
+          {'<'}IoC & DI B Java. Управления зависимостей <br />
+          <br />
+          {'<'}АОР интеграция с Spring
+          <br />
+          <br />
+          {'<'}Бины и их жизненный цикл
+          <br />
+          <br />
+          {'<'}Обработки ошибок и исключений <br /> <br />
+          {'<'}Spring MVC <br />
+          <br />
+          {'<'}Конфигурации Spring MVC приложений <br /> <br />
+          {'<'}Создание контроллеров <br /> <br />
+          {'<'}Аннотации @Controller
+          <br />
+          <br />
+          {'<'}@RequestMapping, @GetMapping, @PostMapping <br /> <br />
+          {'<'}Работа с представлениями <br /> <br />
+          {'<'}Обработка форм <br />
+          <br />
+          {'<'}Работа с файлами и загрузка файлов
+          <br /> <br />
+        </>
+      ),
+    },
+    {
+      title: 'Spring REST API',
+      content: (
+        <>
+          {'<'}НТТР протокол: GET, POST, PUT,
+          <br />
+          <br />
+          {'<'}DELETE
+          <br />
+          <br />
+          {'<'}Фреймворки для создания RESTEul сервисов: Spring Boot, JAX-RS и
+          т.д.
+          <br /> <br />
+          {'<'}Механизмы маршрутизации запросов и обработку URI
+          <br />
+          <br />
+          {'<'}Форматы данных для передачи информации в REST API (JSON & XML)
+          <br /> <br />
+          {'<'}Методы аутентификации и авторизации в REST API
+          <br />
+          <br />
+        </>
+      ),
+    },
+  ]
 
   return (
     <section className={styles.intensive_details}>
-      <div className={styles.intensive_details__heading} >
+      <div className={styles.intensive_details__heading}>
         <p>Подробная программа интенсива</p>
         <div className={styles.buttons_for_scrolling}>
-          <button className={styles.button_to_left} onClick={() => handleLeftButtonClick()}></button>
-          <button className={styles.button_to_right} onClick={() => handleRightButtonClick()}></button>
+          <button
+            className={styles.button_to_left}
+            onClick={() => handleLeftButtonClick()}
+          ></button>
+          <button
+            className={styles.button_to_right}
+            onClick={() => handleRightButtonClick()}
+            disabled={isRightButtonDisabled}
+          ></button>
         </div>
       </div>
-      <div className={styles.intensive_details__main}>
-        <div className={styles.intensive_details__main__display}>
-          <p>001</p>
-          <div className={styles.table_info}>
-            <h1 className={styles.table_info__heading}>Java Core</h1><br />
-            <p className={styles.table_info__text}>
-              {'<'}Code conventions <br /> <br />
-              {'<'}Типы данных, переменные и управляющие конструкции<br /> <br />
-              {'<'}Массивы и коллекции & ООП: инкапсуляция, наследование, полиморфизм и абстракция<br /> <br />
-              {'<'}Исключения<br /> <br />
-              {'<'}Потоки. Использование ключевого слова synchronized и методы wait,<br /> <br />
-              {'<'}notify, notifyAll<br /><br />
-              {'<'}Ввод/вывод (I/0)<br /><br />
-              {'<'}Generics<br /><br />
-            </p>
+      {/* TODO: Вынести position: relative и transition в css */}
+      <div
+        className={styles.intensive_details__main}
+        style={{
+          position: 'relative',
+          left: setctionPosition + 'px',
+          transition: '1s left ease',
+        }}
+      >
+        {details.map(({ title, content }, index) => (
+          <div
+            className={styles.intensive_details__main__display}
+            key={nanoid()}
+            ref={details.length - 1 === index ? lastSectionRef : undefined}
+          >
+            <p>00{++index}</p>
+            <div className={styles.table_info}>
+              <h1 className={styles.table_info__heading}>{title}</h1> <br />
+              <p className={styles.table_info__text}>{content}</p>
+            </div>
           </div>
-        </div>
-
-        <div className={styles.intensive_details__main__display}>
-          <p>002</p>
-          <div className={styles.table_info}>
-            <h1 className={styles.table_info__heading}>Elasticsearch</h1> <br />
-            <p className={styles.table_info__text}>
-              {'<'}Работа с индексами и документами<br /> <br />
-              {'<'}Поиск данных: ключевые слова, фразы, диапазоны значений<br /> <br />
-              {'<'}Фильтры и агрегации - уточнение поискового запроса<br /> <br />
-              {'<'}Многоязычный поиск<br /> <br />
-              {'<'}Поиск с поддержкой релевантности<br /> <br />
-            </p>
-          </div>
-        </div>
-
-        <div className={styles.intensive_details__main__display}>
-          <p>003</p>
-          <div className={styles.table_info}>
-            <h1 className={styles.table_info__heading}>Spring Core & MVC</h1> <br />
-            <p className={styles.table_info__text}>
-              {'<'}Inversion of Control <br /><br />
-              {'<'}Dependecy Injection <br /><br />
-              {'<'}IoC & DI B Java. Управления зависимостей <br /><br />
-              {'<'}АОР интеграция с Spring<br /><br />
-              {'<'}Бины и их жизненный цикл<br /><br />
-              {'<'}Обработки ошибок и исключений <br /> <br />
-              {'<'}Spring MVC <br /><br />
-              {'<'}Конфигурации Spring MVC приложений <br /> <br />
-              {'<'}Создание контроллеров <br /> <br />
-              {'<'}Аннотации @Controller<br /><br />
-              {'<'}@RequestMapping, @GetMapping, @PostMapping <br /> <br />
-              {'<'}Работа с представлениями <br /> <br />
-              {'<'}Обработка форм <br /><br />
-              {'<'}Работа с файлами и загрузка файлов<br /> <br />
-            </p>
-          </div>
-        </div>
-
-        <div className={styles.intensive_details__main__display}>
-          <p>004</p>
-          <div className={styles.table_info}>
-            <h1 className={styles.table_info__heading}>Spring REST API</h1> <br />
-            <p className={styles.table_info__text}>
-              {'<'}НТТР протокол: GET, POST, PUT,<br /><br />
-              {'<'}DELETE<br /><br />
-              {'<'}Фреймворки для создания RESTEul сервисов: Spring Boot, JAX-RS и т.д.<br /> <br />
-              {'<'}Механизмы маршрутизации запросов и обработку URI<br /><br />
-              {'<'}Форматы данных для передачи информации в REST API (JSON & XML)<br /> <br />
-              {'<'}Методы аутентификации и авторизации в REST API<br /><br />
-            </p>
-          </div>
-        </div>
-
+        ))}
       </div>
     </section>
   )
--- src/components/EightBlock/index.tsx
+++ src/components/EightBlock/index.tsx
@@ -1,18 +1,17 @@
 'use client'
 import Image from 'next/image'
 import styles from './styles.module.scss'
-import React, { useState, useEffect, useRef, RefObject } from 'react';
-import classNames from 'classnames';
+import React, { useState, useEffect, useRef, RefObject } from 'react'
+import classNames from 'classnames'
 import arrow_button from 'public/arrow_button.svg'
-
 
 function isInViewport(elementRef: RefObject<HTMLElement>): boolean {
   if (!elementRef || !elementRef.current) {
-    return false;
+    return false
   }
 
-  const element = elementRef.current;
-  const rect = element.getBoundingClientRect();
+  const element = elementRef.current
+  const rect = element.getBoundingClientRect()
 
   return (
     rect.top >= 0 &&
@@ -20,189 +19,183 @@ function isInViewport(elementRef: RefObject<HTMLElement>): boolean {
     rect.bottom <=
       (window.innerHeight || document.documentElement.clientHeight) &&
     rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-  );
+  )
 }
 
 type BlockProps = {
-  title: string;
-  description: string;
-  showInfo: boolean;
-  onClick: () => void;
-  className?: string; 
-  
-};
+  title: string
+  description: string
+  showInfo: boolean
+  onClick: () => void
+  className?: string
+}
 
-const Block = ({ title, description, showInfo, onClick, className }: BlockProps) => {
-   
-  
-    return (
-      <div className={classNames(styles.scroll_blocks, { show: showInfo })} >
-        <h3>{title}</h3>
-        {showInfo ? (
-          <div>
-            <p>{description}</p>
-          </div>
-        ) : (
-            <div>
-          <button onClick={() => {onClick()}} >
+const Block = ({
+  title,
+  description,
+  showInfo,
+  onClick,
+  className,
+}: BlockProps) => {
+  return (
+    <div className={classNames(styles.scroll_blocks, { show: showInfo })}>
+      <h3>{title}</h3>
+      {showInfo ? (
+        <div>
+          <p>{description}</p>
+        </div>
+      ) : (
+        <div>
+          <button
+            onClick={() => {
+              onClick()
+            }}
+          >
             Подробнее
             <Image src={arrow_button} alt="arrow" />
           </button>
-          
-           </div>
-        )}<div className={styles.bottom_color} ></div>
         </div>
-    );
-  };
-
-
-
+      )}
+      <div className={styles.bottom_color}></div>
+    </div>
+  )
+}
 
 const Blocks = () => {
-  const [blockStates, setBlockStates] = useState<Array<boolean>>(Array(9).fill(false));
-  const blockRef = useRef(null);
- // const [isInView, setIsInView] = useState(false);
-  const [isVisible, setIsVisible] = useState(false);
-  const [visibleBlockCount, setVisibleBlockCount] = useState<number>(0);
+  const [blockStates, setBlockStates] = useState<Array<boolean>>(
+    Array(9).fill(false),
+  )
+  const blockRef = useRef(null)
+  // const [isInView, setIsInView] = useState(false);
+  const [isVisible, setIsVisible] = useState(false)
+  const [visibleBlockCount, setVisibleBlockCount] = useState<number>(0)
 
-   const [isScrollingDown, setIsScrollingDown] = useState(false);
-
-  
-
+  const [isScrollingDown, setIsScrollingDown] = useState(false)
 
   useEffect(() => {
     const handleScroll = () => {
-    //  setIsInView(isInViewport(blockRef));
-      setIsVisible(isInViewport(blockRef));
-    };
+      //  setIsInView(isInViewport(blockRef));
+      setIsVisible(isInViewport(blockRef))
+    }
 
-     if (isVisible) {
+    if (isVisible) {
       // Отключаем скроллинг страницы, когда блок виден
-      document.body.style.overflow = 'hidden';
+      document.body.style.overflow = 'hidden'
     } else {
       // Восстанавливаем обычный скроллинг, когда блок не виден
-      document.body.style.overflow = 'auto';
+      document.body.style.overflow = 'auto'
     }
 
     // Добавляем обработчик события прокрутки
-    window.addEventListener("scroll", handleScroll);
+    window.addEventListener('scroll', handleScroll)
 
     // Проверяем при загрузке страницы
-    handleScroll();
+    handleScroll()
 
     // Убираем обработчик события при размонтировании компонента
     return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, [isVisible]);
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, [isVisible])
 
-
-  
-    
-   
-   
-   
-   
-   const handleBlockClick = (index: number) => {
-      const updatedBlockStates = blockStates.map((state, i) => (i === index ? !state : false));
-      setBlockStates(updatedBlockStates);
-    };
-
-    return (      
-       <div>
-       <section className={styles.learn_program} >
-            <div className={styles.learn_program__heading}>
-                <div></div>
-                <h1>программа обучения</h1>
-                <div></div>
-            </div>
-
-            <div className={styles.learn_program__main} ref={blockRef}>
-                <h2>8 блоков обучения</h2>
-                <p>Интенсив захватывает основы языка <br /> программирования Java, работу <br />
-                с различными инструментами <br />
-                и библиотеками, а также непосредственно <br /> разработку веб-приложений</p>
-                <div className={styles.bottom_section}>
-                    <div className={styles.bottom_section__figure}></div>
-                    <div className={styles.bottom_section__scrolling} >
-
-            <Block
-              title="Java Core"
-              description="База, которая необходима для старта. На интенсиве изучаем основы программирования и на практике 
-              работаем с Java Core: интеграции с другими технологиями, 
-              многопоточные программировании, разработка приложений и др."
-              showInfo={blockStates[0]}
-              onClick={() => handleBlockClick(0)}
-            />        
- 
-            <Block
-              title="Java Spring"
-              description="Описание для Java Spring."
-              showInfo={blockStates[1]}
-              onClick={() => handleBlockClick(1)}
-            />
-
-            <Block
-              title="Elasticsearch"
-              description="Описание для Elasticsearch."
-              showInfo={blockStates[2]}
-              onClick={() => handleBlockClick(2)}
-            />
-
-          <Block
-              title="Spring Core & MVC"
-              description="Описание для Java Spring."
-              showInfo={blockStates[3]}
-              onClick={() => handleBlockClick(3)}
-            />
-
-            <Block
-              title="Spring REST API"
-              description="Описание для Elasticsearch."
-              showInfo={blockStates[4]}
-              onClick={() => handleBlockClick(4)}
-            />
-
-            <Block
-              title="Spring OpenAPI"
-              description="Описание для Java Spring."
-              showInfo={blockStates[5]}
-              onClick={() => handleBlockClick(5)}
-            />
-
-            <Block
-              title="JUnit Tests"
-              description="Описание для Elasticsearch."
-              showInfo={blockStates[6]}
-              onClick={() => handleBlockClick(6)}
-            />
-             <Block
-              title="Spring Data"
-              description="Описание для Java Spring."
-              showInfo={blockStates[7]}
-              onClick={() => handleBlockClick(7)}
-            />
-
-            <Block
-              title="Spring Security"
-              description="Описание для Elasticsearch."
-              showInfo={blockStates[8]}
-              onClick={() => handleBlockClick(8)}
-              className={styles.last_block}
-              
-            />
-                  </div>
-                </div>
-            </div>
-           
-       </section> 
-      </div>  
+  const handleBlockClick = (index: number) => {
+    const updatedBlockStates = blockStates.map((state, i) =>
+      i === index ? !state : false,
     )
-  
+    setBlockStates(updatedBlockStates)
   }
 
+  return (
+    <div>
+      <section className={styles.learn_program}>
+        <div className={styles.learn_program__heading}>
+          <div></div>
+          <h1>программа обучения</h1>
+          <div></div>
+        </div>
 
-    export default Blocks 
+        <div className={styles.learn_program__main} ref={blockRef}>
+          <h2>8 блоков обучения</h2>
+          <p>
+            Интенсив захватывает основы языка <br /> программирования Java,
+            работу <br />
+            с различными инструментами <br />
+            и библиотеками, а также непосредственно <br /> разработку
+            веб-приложений
+          </p>
+          <div className={styles.bottom_section}>
+            <div className={styles.bottom_section__figure}></div>
+            <div className={styles.bottom_section__scrolling}>
+              <Block
+                title="Java Core"
+                description="База, которая необходима для старта. На интенсиве изучаем основы программирования и на практике 
+              работаем с Java Core: интеграции с другими технологиями, 
+              многопоточные программировании, разработка приложений и др."
+                showInfo={blockStates[0]}
+                onClick={() => handleBlockClick(0)}
+              />
 
+              <Block
+                title="Java Spring"
+                description="Описание для Java Spring."
+                showInfo={blockStates[1]}
+                onClick={() => handleBlockClick(1)}
+              />
 
+              <Block
+                title="Elasticsearch"
+                description="Описание для Elasticsearch."
+                showInfo={blockStates[2]}
+                onClick={() => handleBlockClick(2)}
+              />
 
+              <Block
+                title="Spring Core & MVC"
+                description="Описание для Java Spring."
+                showInfo={blockStates[3]}
+                onClick={() => handleBlockClick(3)}
+              />
+
+              <Block
+                title="Spring REST API"
+                description="Описание для Elasticsearch."
+                showInfo={blockStates[4]}
+                onClick={() => handleBlockClick(4)}
+              />
+
+              <Block
+                title="Spring OpenAPI"
+                description="Описание для Java Spring."
+                showInfo={blockStates[5]}
+                onClick={() => handleBlockClick(5)}
+              />
+
+              <Block
+                title="JUnit Tests"
+                description="Описание для Elasticsearch."
+                showInfo={blockStates[6]}
+                onClick={() => handleBlockClick(6)}
+              />
+              <Block
+                title="Spring Data"
+                description="Описание для Java Spring."
+                showInfo={blockStates[7]}
+                onClick={() => handleBlockClick(7)}
+              />
+
+              <Block
+                title="Spring Security"
+                description="Описание для Elasticsearch."
+                showInfo={blockStates[8]}
+                onClick={() => handleBlockClick(8)}
+                className={styles.last_block}
+              />
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  )
+}
+
+export default Blocks
