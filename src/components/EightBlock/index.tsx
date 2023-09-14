d05ead7d177d9ae52e725023fdc16f4822d0035e
Kirill Bushmanov <bushmanov.k@qazdevelop.com>
2023-09-14 16:47:33 +0600

    Study program blocks animation

--- src/components/EightBlock/index.tsx
+++ src/components/EightBlock/index.tsx
@@ -1,33 +1,201 @@
 'use client'
+import React, { RefObject, useEffect, useRef, useState } from 'react'
 import Image from 'next/image'
-import styles from './styles.module.scss'
-import React, { useState, useEffect, useRef, RefObject } from 'react'
 import classNames from 'classnames'
+import { nanoid } from 'nanoid'
 import arrow_button from 'public/arrow_button.svg'
 
-function isInViewport(elementRef: RefObject<HTMLElement>): boolean {
-  if (!elementRef || !elementRef.current) {
-    return false
+import styles from './styles.module.scss'
+
+const Blocks = () => {
+  const BLOCK_HEIGHT = 205
+
+  const ref = useRef<any>(null)
+  const [blocks, setBlocks] = useState([
+    {
+      title: 'Java Core',
+      description:
+        'База, которая необходима для старта. На интенсиве изучаем основы программирования и на практике работаем с Java Core: интеграции с другими технологиями, многопоточные программировании, разработка приложений и др.',
+      showInfo: false,
+    },
+    {
+      title: 'Java Spring',
+      description: 'Описание для Java Spring.',
+      showInfo: false,
+    },
+    {
+      title: 'Elasticsearch',
+      description: 'Описание для Elasticsearch.',
+      showInfo: false,
+    },
+    {
+      title: 'Spring Core & MVC',
+      description: 'Описание для Java Spring.',
+      showInfo: false,
+    },
+    {
+      title: 'Spring REST API',
+      description: 'Описание для Elasticsearch.',
+      showInfo: false,
+    },
+    {
+      title: 'Spring OpenAPI',
+      description: 'Описание для Java Spring.',
+      showInfo: false,
+    },
+    {
+      title: 'JUnit Tests',
+      description: 'Описание для Elasticsearch.',
+      showInfo: false,
+    },
+    {
+      title: 'Spring Data',
+      description: 'Описание для Java Spring.',
+      showInfo: false,
+    },
+    {
+      title: 'Spring Security',
+      description: 'Описание для Elasticsearch.',
+      showInfo: false,
+    },
+  ])
+  const [isVisibleSection, setIsVisibleSection] = useState(false)
+  const [scrollsBottomCount, setScrollsBottomCount] = useState(0)
+
+  useEffect(() => {
+    const handleScroll = (event: any) => {
+      setIsVisibleSection(isInViewport(ref))
+    }
+    window.addEventListener('scroll', handleScroll)
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, [])
+
+  useEffect(() => {
+    if (isVisibleSection) {
+      let scrollTimeout: any = null
+      const handleBodyScroll = (e: WheelEvent) => {
+        if (scrollsBottomCount < blocks.length - 1 && e.deltaY > 0) {
+          e.preventDefault()
+
+          if (scrollTimeout) {
+            clearTimeout(scrollTimeout)
+          }
+
+          scrollTimeout = setTimeout(() => {
+            const currentScrollPosition =
+              window.pageYOffset || document.documentElement.scrollTop
+
+            const newScrollPosition = currentScrollPosition + BLOCK_HEIGHT
+
+            window.scrollTo({
+              top: newScrollPosition,
+              behavior: 'smooth',
+            })
+
+            setScrollsBottomCount(scrollsBottomCount + 1)
+            scrollTimeout = null
+          }, 300)
+        }
+      }
+
+      document.body.addEventListener('wheel', handleBodyScroll, {
+        passive: false,
+      })
+
+      return () => {
+        document.body.removeEventListener('wheel', handleBodyScroll)
+      }
+    }
+  }, [isVisibleSection, scrollsBottomCount])
+
+  const handleBlockClick = (index: number) => {
+    setBlocks((prevBlocks) => {
+      const updatedBlocks = [...prevBlocks]
+      updatedBlocks[index].showInfo = true
+      return updatedBlocks
+    })
   }
 
-  const element = elementRef.current
-  const rect = element.getBoundingClientRect()
+  function isInViewport(elementRef: RefObject<HTMLElement>): boolean {
+    if (!elementRef || !elementRef.current) {
+      return false
+    }
+
+    const element = elementRef.current
+    const rect = element.getBoundingClientRect()
+
+    return (
+      rect.bottom >= 0 &&
+      rect.bottom <=
+        (window.innerHeight || document.documentElement.clientHeight)
+    )
+  }
 
   return (
-    rect.top >= 0 &&
-    rect.left >= 0 &&
-    rect.bottom <=
-      (window.innerHeight || document.documentElement.clientHeight) &&
-    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    <div>
+      <section className={styles.learn_program}>
+        <div className={classNames(styles.learn_program__heading, 'container')}>
+          <h1>программа обучения</h1>
+        </div>
+        <div className="container">
+          <div className={styles.learn_program__main}>
+            <h2>8 блоков обучения</h2>
+            <p>
+              Интенсив захватывает основы языка <br /> программирования Java,
+              работу <br />
+              с различными инструментами <br />
+              и библиотеками, а также непосредственно <br /> разработку
+              веб-приложений
+            </p>
+            <div className={styles.bottom_section}>
+              <div className={styles.bottom_section__figure}></div>
+              <div
+                className={styles.bottom_section__scrolling}
+                ref={ref}
+                style={{
+                  height: (scrollsBottomCount + 1) * BLOCK_HEIGHT,
+                }}
+              >
+                {blocks.map((blockProps, index) => {
+                  return (
+                    <div key={nanoid()}>
+                      <Block
+                        className={
+                          blocks.length - 1 === index ? styles.last_block : ''
+                        }
+                        top={
+                          (index !== 0 &&
+                            index <= scrollsBottomCount &&
+                            `${index * BLOCK_HEIGHT}px`) ||
+                          ''
+                        }
+                        index={index}
+                        onClick={() => handleBlockClick(index)}
+                        {...blockProps}
+                      />
+                    </div>
+                  )
+                })}
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
   )
 }
 
 type BlockProps = {
   title: string
   description: string
-  showInfo: boolean
+  showInfo?: boolean
   onClick: () => void
   className?: string
+  top: any
+  index: number
 }
 
 const Block = ({
@@ -36,9 +204,15 @@ const Block = ({
   showInfo,
   onClick,
   className,
+  top,
+  index,
 }: BlockProps) => {
   return (
-    <div className={classNames(styles.scroll_blocks, { show: showInfo })}>
+    <div
+      className={classNames(styles.scroll_blocks, { show: showInfo })}
+      style={{ top: `${top}` }}
+      id={`block-${index}`}
+    >
       <h3>{title}</h3>
       {showInfo ? (
         <div>
@@ -57,142 +231,6 @@ const Block = ({
         </div>
       )}
       <div className={styles.bottom_color}></div>
-    </div>
-  )
-}
-
-const Blocks = () => {
-  const [blockStates, setBlockStates] = useState<Array<boolean>>(
-    Array(9).fill(false),
-  )
-  const blockRef = useRef(null)
-  // const [isInView, setIsInView] = useState(false);
-  const [isVisible, setIsVisible] = useState(false)
-  const [visibleBlockCount, setVisibleBlockCount] = useState<number>(0)
-
-  const [isScrollingDown, setIsScrollingDown] = useState(false)
-
-  useEffect(() => {
-    const handleScroll = () => {
-      //  setIsInView(isInViewport(blockRef));
-      setIsVisible(isInViewport(blockRef))
-    }
-
-    if (isVisible) {
-      // Отключаем скроллинг страницы, когда блок виден
-      document.body.style.overflow = 'hidden'
-    } else {
-      // Восстанавливаем обычный скроллинг, когда блок не виден
-      document.body.style.overflow = 'auto'
-    }
-
-    // Добавляем обработчик события прокрутки
-    window.addEventListener('scroll', handleScroll)
-
-    // Проверяем при загрузке страницы
-    handleScroll()
-
-    // Убираем обработчик события при размонтировании компонента
-    return () => {
-      window.removeEventListener('scroll', handleScroll)
-    }
-  }, [isVisible])
-
-  const handleBlockClick = (index: number) => {
-    const updatedBlockStates = blockStates.map((state, i) =>
-      i === index ? !state : false,
-    )
-    setBlockStates(updatedBlockStates)
-  }
-
-  return (
-    <div>
-      <section className={styles.learn_program}>
-        <div className={classNames(styles.learn_program__heading, 'container')}>
-          <h1>программа обучения</h1>
-        </div>
-        <div className="container">
-          <div className={styles.learn_program__main} ref={blockRef}>
-            <h2>8 блоков обучения</h2>
-            <p>
-              Интенсив захватывает основы языка <br /> программирования Java,
-              работу <br />
-              с различными инструментами <br />
-              и библиотеками, а также непосредственно <br /> разработку
-              веб-приложений
-            </p>
-            <div className={styles.bottom_section}>
-              <div className={styles.bottom_section__figure}></div>
-              <div className={styles.bottom_section__scrolling}>
-                <Block
-                  title="Java Core"
-                  description="База, которая необходима для старта. На интенсиве изучаем основы программирования и на практике 
-              работаем с Java Core: интеграции с другими технологиями, 
-              многопоточные программировании, разработка приложений и др."
-                  showInfo={blockStates[0]}
-                  onClick={() => handleBlockClick(0)}
-                />
-
-                <Block
-                  title="Java Spring"
-                  description="Описание для Java Spring."
-                  showInfo={blockStates[1]}
-                  onClick={() => handleBlockClick(1)}
-                />
-
-                <Block
-                  title="Elasticsearch"
-                  description="Описание для Elasticsearch."
-                  showInfo={blockStates[2]}
-                  onClick={() => handleBlockClick(2)}
-                />
-
-                <Block
-                  title="Spring Core & MVC"
-                  description="Описание для Java Spring."
-                  showInfo={blockStates[3]}
-                  onClick={() => handleBlockClick(3)}
-                />
-
-                <Block
-                  title="Spring REST API"
-                  description="Описание для Elasticsearch."
-                  showInfo={blockStates[4]}
-                  onClick={() => handleBlockClick(4)}
-                />
-
-                <Block
-                  title="Spring OpenAPI"
-                  description="Описание для Java Spring."
-                  showInfo={blockStates[5]}
-                  onClick={() => handleBlockClick(5)}
-                />
-
-                <Block
-                  title="JUnit Tests"
-                  description="Описание для Elasticsearch."
-                  showInfo={blockStates[6]}
-                  onClick={() => handleBlockClick(6)}
-                />
-                <Block
-                  title="Spring Data"
-                  description="Описание для Java Spring."
-                  showInfo={blockStates[7]}
-                  onClick={() => handleBlockClick(7)}
-                />
-
-                <Block
-                  title="Spring Security"
-                  description="Описание для Elasticsearch."
-                  showInfo={blockStates[8]}
-                  onClick={() => handleBlockClick(8)}
-                  className={styles.last_block}
-                />
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
     </div>
   )
 }
