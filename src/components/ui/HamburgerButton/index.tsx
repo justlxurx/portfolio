572a05e7b9d2a2c2142726c5a06bdffaf5e36dd1
laura <justlxurx0@gmail.com>
2023-09-15 17:47:55 +0600

    comments

--- src/components/ui/HamburgerButton/index.tsx
+++ src/components/ui/HamburgerButton/index.tsx
@@ -1,12 +1,4 @@
-// import styles from './styles.module.scss'
-
-// const HamburgerButton = () =>
-// <button className={styles.hamburger_button} />
-
-// export default HamburgerButton
-
 'use client'
-
 import styles from './styles.module.scss'
 import Burger from '@/components/Burger'
 import React, { useState } from 'react'
