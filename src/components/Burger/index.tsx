7fabb055d7627a75c13ac7fe4b843ea38d185465
laura <justlxurx0@gmail.com>
2023-09-18 11:21:25 +0600

    burger component is in progress

--- src/components/Burger/index.tsx
+++ src/components/Burger/index.tsx
@@ -1,13 +1,24 @@
 import { nanoid } from 'nanoid'
-
-import FeedbackButton from '../ui/FeedbackButton'
+import React, { useState } from 'react'
 //import HamburgerButton from '../ui/HamburgerButton'
 import NavButton from '../ui/NavButton/index'
 import { INavButton } from '../ui/NavButton/interface'
 
 import styles from './styles.module.scss'
 
+interface BurgerProps {
+  visible: boolean // Укажите тип параметра visible как boolean
+}
+
 const Navbar = () => {
+  const [isVisible, setIsVisible] = useState(true)
+  const toggleNavbarVisibility = (visible: boolean) => {
+    setIsVisible(visible)
+  }
+  const handleNavbarClose = () => {
+    toggleNavbarVisibility(false)
+  }
+
   const buttons: INavButton[] = [
     {
       title: 'Devcorn',
@@ -28,25 +39,32 @@ const Navbar = () => {
   ]
 
   return (
-    <section className={styles.burger}>
-      <div className={styles.burger__buttons_wrapper}>
-        <button className={styles.close_button}></button>
+    <div>
+      {isVisible && (
+        <section className={styles.burger}>
+          <div className={styles.burger__buttons_wrapper}>
+            <button
+              className={styles.close_button}
+              onClick={handleNavbarClose}
+            ></button>
 
-        <ul>
-          {buttons.map((button: INavButton) => (
-            <li key={nanoid()}>
-              <NavButton {...button} />
-            </li>
-          ))}
-        </ul>
-        {/* <div className={styles.feedback_button}>
+            <ul>
+              {buttons.map((button: INavButton) => (
+                <li key={nanoid()}>
+                  <NavButton {...button} />
+                </li>
+              ))}
+            </ul>
+            {/* <div className={styles.feedback_button}>
           <FeedbackButton />
         </div> */}
-        <button className={styles.button_to_call}>
-          <a href={'tel:+77212940165'}>связаться с нами</a>
-        </button>
-      </div>
-    </section>
+            <button className={styles.button_to_call}>
+              <a href={'tel:+77212940165'}>связаться с нами</a>
+            </button>
+          </div>
+        </section>
+      )}
+    </div>
   )
 }
 
